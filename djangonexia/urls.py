aa95c26914bc762c73868bd670778e14418bec7b
Jesus Erick Añez <peluchin.salas2000@gmail.com>
2026-05-26 19:40:43 -0400

    Keep clean main urls deployment

--- djangonexia/urls.py
+++ djangonexia/urls.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.views.static import serve
-from django.urls import re_path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # <-- CORREGIDO AQUÍ (Antes decía split)
-    path('', include('tasks.urls')),  # Rutas de tu app
+    path('admin/', admin.site.urls),
+    path('', include('tasks.urls')),
 ]
-
-# Forzar a Django a servir los archivos estáticos en Render cuando DEBUG = False
-if not settings.DEBUG:
-    urlpatterns += [
-        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    ]
