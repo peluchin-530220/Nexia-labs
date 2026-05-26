ea023f00fbe8a7aa944a2fb3002e22a64cd4704f
Jesus Erick Añez <peluchin.salas2000@gmail.com>
2026-05-26 19:27:37 -0400

    Force static files serving via urls routing for production

--- djangonexia/urls.py
+++ djangonexia/urls.py
@@ -1,23 +1,22 @@
-"""
-URL configuration for djangonexia project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from tasks import views
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
+urlpatterns = [
+    path('admin/', admin.site.split), # Revisa si tu ruta de admin se llama admin/ o tiene otro nombre
+    path('', include('tasks.urls')),   # Las rutas de tu aplicación local
+]
+
+# FORZAR A DJANGO A SERVIR ESTÁTICOS EN PRODUCCIÓN (RENDER)
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?H.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns = [
     path('', views.index, name='index'),
     path('contacto/', views.contacto, name='contacto'),
