c50705f7fcb57fdd38f0405819d840c60daa49f8
Jesus Erick Añez <peluchin.salas2000@gmail.com>
2026-05-26 19:21:53 -0400

    Add custom build script to force collectstatic on render

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+# Salir inmediatamente si ocurre un error
+set -o errexit
 
+# 1. Instalar dependencias
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# 2. Forzar la recopilación limpia de archivos estáticos (Esto creará la carpeta para tu logo)
+python manage.py collectstatic --no-input --clear
+
+# 3. Aplicar migraciones de base de datos (por si acaso)
 python manage.py migrate
