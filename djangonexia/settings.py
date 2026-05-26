ae5838cb4e1f4029edbf3ba4edd28249058ce8fa
Jesus Erick Añez <peluchin.salas2000@gmail.com>
2026-05-26 13:51:22 -0400

    Fix render deploy

--- djangonexia/settings.py
+++ djangonexia/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from pathlib import Path
 import os
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
