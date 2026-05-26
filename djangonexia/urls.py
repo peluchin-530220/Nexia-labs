e6ace22110dc5e2c882e012c907d7c15079ac630
Jesus Erick Añez <peluchin.salas2000@gmail.com>
2026-05-26 19:33:01 -0400

    Fix urls typo and establish stable static route

--- djangonexia/urls.py
+++ djangonexia/urls.py
@@ -1,25 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
 from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns = [
-    path('admin/', admin.site.split), # Revisa si tu ruta de admin se llama admin/ o tiene otro nombre
-    path('', include('tasks.urls')),   # Las rutas de tu aplicación local
+    path('admin/', admin.site.urls),  # <-- CORREGIDO AQUÍ (Antes decía split)
+    path('', include('tasks.urls')),  # Rutas de tu app
 ]
 
-# FORZAR A DJANGO A SERVIR ESTÁTICOS EN PRODUCCIÓN (RENDER)
+# Forzar a Django a servir los archivos estáticos en Render cuando DEBUG = False
 if not settings.DEBUG:
     urlpatterns += [
-        re_path(r'^static/(?H.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('sobre/', views.sobre, name='sobre'),
-    path('admin/', admin.site.urls),
-]
